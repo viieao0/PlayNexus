ed9f62efc39239e391e518ece6c75c4d828b8d1b
viieao0 <viieao0@example.com>
2026-01-30 02:46:49 +0300

    style(db): improve readability of database setup

--- backend/database.py
+++ backend/database.py
@@ -4,9 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config.environment import db_URI
 #create
+
 engine = create_engine(db_URI)
 
 SessionLocal = sessionmaker(
+    
     autocommit=False,
     autoflush=False,
     bind=engine
